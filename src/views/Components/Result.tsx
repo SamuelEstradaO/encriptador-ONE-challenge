aceb7209517d219989072bcf800031221fed9262
SamuelEstradaO <sy.estrada.o@gmail.com>
2023-05-17 02:07:25 -0600

    fix error on clipboard

--- src/views/Components/Result.tsx
+++ src/views/Components/Result.tsx
@@ -26,14 +26,13 @@ interface Props {
 
 }
 const Result: FunctionComponent<Props> = ({ result }) => {
-    console.log(result);
     const [value, copy] = useCopyToClipboard();
     const [showButton, setShowButton] = useState<boolean>(true);
     const [error, setError] = useState<boolean>(false)
     const ref = useRef<HTMLTextAreaElement | null>(null)
     const handleClick = async (event: React.MouseEvent<HTMLButtonElement>) => {
         const copyResult = await copy(ref.current?.textContent || "");
-        copyResult ? setError(false) : setError(true);
+        setError(!copyResult);
         setShowButton(false);
         setTimeout(() => {
             setShowButton(true)
@@ -42,6 +41,8 @@ const Result: FunctionComponent<Props> = ({ result }) => {
     }
     return (
         <Container className="col-sm-12 col-md-4 mb-sm-2 mb-0">
+            <div className="z-3 bg-black">
+                <p>{error}</p></div>
             <div className="container-fluid position-relative mx-0 row h-100">
                 {result.length === 0 && <Div className="p-4 col-12">
                     <img src="/the_search.svg" alt="" className="img-fluid mb-1 d-none d-md-block" style={{ objectFit: "contain" }} />
@@ -52,8 +53,7 @@ const Result: FunctionComponent<Props> = ({ result }) => {
                     <Textarea readOnly className="p-2 position-absolute bottom-0 start-50 translate-middle-x" ref={ref} value={result}></Textarea>
                     {showButton && <Button className="badge rounded-pill py-2 mb-2 position-absolute bottom-0 start-50 translate-middle-x" onClick={handleClick}>Copiar</Button>}
                     {!showButton && !error && <P className="user-select-none badge rounded-pill py-2 mb-2 position-absolute bottom-0 start-50 translate-middle-x">Copiado</P>}
-                    {!showButton && error && <P className="user-select-none badge rounded-pill text-bg-danger py-2 mb-2 position-absolute bottom-0 start-50 translate-middle-x w-75">Copiado</P>}
-
+                    {!showButton && error && <P className="user-select-none badge rounded-pill text-bg-danger py-2 mb-2 position-absolute bottom-0 start-50 translate-middle-x w-50">Error</P>}
                 </div>}
             </div>
 
